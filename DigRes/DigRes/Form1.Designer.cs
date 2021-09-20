86b83390045768c8bfa6635da127450dbaeec731
Neven <58472164+neven14@users.noreply.github.com>
2021-09-20 09:00:50 +0200

    Add files via upload

---
+++ DigRes/DigRes/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DigRes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonOtvori = new System.Windows.Forms.Button();
+            this.textboxRezultat = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonOtvori
+            // 
+            this.buttonOtvori.Location = new System.Drawing.Point(327, 105);
+            this.buttonOtvori.Name = "buttonOtvori";
+            this.buttonOtvori.Size = new System.Drawing.Size(179, 60);
+            this.buttonOtvori.TabIndex = 0;
+            this.buttonOtvori.Text = "OtvoriteMessageBox";
+            this.buttonOtvori.UseVisualStyleBackColor = true;
+            this.buttonOtvori.Click += new System.EventHandler(this.ButtonOtvori_Click);
+            // 
+            // textboxRezultat
+            // 
+            this.textboxRezultat.Location = new System.Drawing.Point(305, 194);
+            this.textboxRezultat.Multiline = true;
+            this.textboxRezultat.Name = "textboxRezultat";
+            this.textboxRezultat.Size = new System.Drawing.Size(238, 95);
+            this.textboxRezultat.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textboxRezultat);
+            this.Controls.Add(this.buttonOtvori);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonOtvori;
+        private System.Windows.Forms.TextBox textboxRezultat;
+    }
+}
+
---
+++ DigRes/DigRes/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigRes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonOtvori_Click(object sender, EventArgs e)
+        {
+            string poruka = "Odaberite Yes / No / Cancel";
+
+            string naslov = "Naslovna linija Dijaloga za poruke";
+
+            MessageBoxButtons buttons = MessageBoxButtons.YesNoCancel;
+
+            MessageBoxIcon ikona = MessageBoxIcon.Information;
+
+            DialogResult rez = MessageBox.Show(poruka, naslov, buttons, ikona);
+
+            switch (rez)
+
+            {
+
+                case DialogResult.Yes:
+
+                    textboxRezultat.Text = "Izabrali ste dugme YES";
+
+                    break;
+
+                case DialogResult.No:
+
+                    textboxRezultat.Text = "Izabrali ste dugme NO";
+
+                    break;
+
+                case DialogResult.Cancel:
+
+                    textboxRezultat.Text = "Izabrali ste dugme Cancel";
+
+                    break;
+
+            }
+        }
+    }
+}
